b74de3b45549a4b6fd42b843a8d3cc5f932abd76
RioMulya <riomulya75@gmail.com>
2022-03-28 23:01:42 +0700

    Tugas 4 php

--- tugas4.php
+++ tugas4.php
@@ -8,4 +8,13 @@ print("jual = $jual<br/>");
 print("keuntungan = $keuntungan<br/>");
 $keuntungan = ($beli - $jual) / $keuntungan;
 
-printf("Berapa Jumlah Kambing yang dibeli ?<br/>Jawaban : %.2f ekor", $keuntungan);
+printf("Berapa Jumlah Kambing yang dibeli ?<br/>Jawaban : %.2f ekor<br/><br/><br/>", $keuntungan);
+
+
+$saluran = 523;
+$terpakai = 8891;
+print("Jumlah saluran = $saluran<br/>");
+print("Jumlah terpakai = $terpakai<br/>");
+$terpakai = $terpakai / $saluran;
+print("Rata-Rata terpakai  ?<br/>");
+print("Jawaban =  $terpakai liter/keluarga<br/>");
